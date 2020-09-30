e97b63a1ad7ce19bc3d4f43be561176d80595f5c
jonec76 <jonec9676@gmail.com>
2020-09-30 10:49:05 -0400

    Fix the timer bug

--- greedy.cpp
+++ greedy.cpp
@@ -16,7 +16,7 @@ extern char OUTPUT_FILE[30];
 clock_t total_start, total_end;
 
 int main(int argc, char **argv){
-    total_end = clock();
+    total_start = clock();
     Graph g;
     if(argc != 2){
         printf("Wrong argument. Execution format: ./main config.txt\n");
