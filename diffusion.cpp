6331de0b159b0cfcd8b7c3cf6f891bb6e9cd623c
jonec76 <jonec9676@gmail.com>
2020-10-07 16:36:43 +0800

    Handle empty strategy case in diffusion

--- diffusion.cpp
+++ diffusion.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <stdlib.h>
+#include <assert.h> 
 #include "graph.h"
 #include "diff_func.h"
 using namespace std;
@@ -27,12 +28,11 @@ double diffusion(vector<vector<struct X> > Strategy, int sam_size, Graph& g){
         vector<vector<struct node*>*> positive_group{&infected, &ailing, &threatened};
         vector<vector<struct node*>*> health_group{&susceptible, &infected, &recovered};
         
+        assert(Strategy.size() == period_T);
         
         vector<struct node*> tmp_susceptible, tmp_infected, tmp_ailing, tmp_threatened, tmp_recovered, tmp_dead;
         vector<vector<struct node*>*> tmp_group{&tmp_infected, &tmp_ailing, &tmp_threatened, &tmp_dead, &tmp_recovered};// Shall align the order of total_group 
         for(size_t t=0;t<period_T;t++){// Quarantine
-            if(Strategy[t].size() == 0)
-                continue;
             g.set_node_lv(Strategy[t]);
             // ====check lv of overlap node (g.N[i]->q_level)===";
             for(size_t i=0;i<positive_group.size();i++){ // infected, ailing, threatened
@@ -70,9 +70,10 @@ double diffusion_greedy(vector<vector<struct X> > Strategy, int sam_size, Graph&
         
         vector<struct node*> tmp_susceptible, tmp_infected, tmp_ailing, tmp_threatened, tmp_recovered, tmp_dead;
         vector<vector<struct node*>*> tmp_group{&tmp_infected, &tmp_ailing, &tmp_threatened, &tmp_dead, &tmp_recovered};// Shall align the order of total_group 
+        
+        assert(Strategy.size() == period_T);
+
         for(size_t t=0;t<period_T;t++){// Quarantine
-            if(Strategy[t].size() == 0)
-                continue;
             g.set_node_lv(Strategy[t]);
             // ====check lv of overlap node (g.N[i]->q_level)===";
             for(size_t i=0;i<positive_group.size();i++){ // infected, ailing, threatened
