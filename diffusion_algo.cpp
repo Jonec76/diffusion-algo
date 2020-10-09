21403b66e2467166fb10faded0f24b6ca512549f
jonec76 <jonec9676@gmail.com>
2020-10-09 19:59:12 +0800

    Add task 2 R0 array
    - new_I_num / IAT_num

--- diffusion_algo.cpp
+++ diffusion_algo.cpp
@@ -11,11 +11,14 @@ extern size_t sample_size, period_T;
 double diffusion_full_result(vector<vector<struct X> > Strategy, Graph& g){
     long double f = 0;
     srand(time(0));
+    double R0_num[period_T] = {0}, susceptible_num[period_T] = {0},  infected_num[period_T] = {0}, ailing_num[period_T] = {0}, threatened_num[period_T] = {0}, recovered_num[period_T] = {0}, dead_num[period_T] = {0};
+
     for(size_t i=0;i<sample_size;i++){
         vector<struct node*>susceptible, infected, ailing, threatened, recovered, dead;
         vector<vector<struct node*>*> total_group{&infected, &ailing, &threatened, &dead, &recovered}; 
         vector<vector<struct node*>*> all_group{&susceptible, &infected, &ailing, &threatened, &dead, &recovered}; 
         susceptible = g.N;
+        double prev_I_num;
         
         for(size_t j=0;j<g.V;j++){ // Init stage
             g.N[j]->stage = Stage::susceptible;
@@ -32,6 +35,7 @@ double diffusion_full_result(vector<vector<struct X> > Strategy, Graph& g){
         
         vector<struct node*> tmp_susceptible, tmp_infected, tmp_ailing, tmp_threatened, tmp_recovered, tmp_dead;
         vector<vector<struct node*>*> tmp_group{&tmp_infected, &tmp_ailing, &tmp_threatened, &tmp_dead, &tmp_recovered};// Shall align the order of total_group 
+        prev_I_num = infected.size();
         for(size_t t=0;t<period_T;t++){// Quarantine
             g.set_node_lv(Strategy[t]);
             // ====check lv of overlap node (g.N[i]->q_level)===";
@@ -44,8 +48,19 @@ double diffusion_full_result(vector<vector<struct X> > Strategy, Graph& g){
             }
             tmp_push_back(tmp_group, total_group);
             f += objective_at_t(health_group, Strategy[t], g.V, g.N);
+            double IAT_num = get_positive_count(positive_group);
+            double new_I_num = infected.size()-prev_I_num;
+            prev_I_num = infected.size();
+            R0_num[t] += (new_I_num / IAT_num);
         }
     }
+
+    // TODO: divide by sample size
+    for(size_t t=0;t<period_T;t++){
+        R0_num[t] /= sample_size;
+        cout<<R0_num[t]<<endl;
+    }
+
     return f/(double)sample_size;
 }
 
